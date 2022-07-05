1855d9401fba256c26c0480989d52b4348beb325
LeastKIds <leastkids@gmail.com>
2022-07-05 14:03:27 +0900

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    // JSX(Javascript Syntax Extension) code return
+    return (
+        <div>
+            <div>Kim JinHong</div><div>Hello</div>
+        </div>
+    )
+}
+export  default Header
